000eba63e46b4d6a1d9e20c8ed7a06b4da9b99f6
kuizuo <hi@kuizuo.cn>
2023-02-27 22:28:26 +0800

    chore(deps): add naive-ui-nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 import { defineNuxtConfig } from 'nuxt/config'
-import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineNuxtConfig({
   app: {
@@ -17,6 +15,7 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxt/content',
     '@pinia-plugin-persistedstate/nuxt',
+    'naive-ui-nuxt',
     '@nuxt/devtools',
   ],
   content: {
@@ -60,40 +59,6 @@ export default defineNuxtConfig({
   },
   typescript: {
     shim: false,
-  },
-  // https://github.com/nuxt/framework/issues/6204#issuecomment-1201398080
-  hooks: {
-    'vite:extendConfig': function (config: any, { isServer }: any) {
-      if (isServer) {
-        // Workaround for netlify issue
-        // https://github.com/nuxt/framework/issues/6204
-        config.build.rollupOptions.output.inlineDynamicImports = true
-      }
-    },
-  },
-  build: {
-    transpile:
-      process.env.NODE_ENV === 'production'
-        ? [
-            'naive-ui',
-            'vueuc',
-            '@css-render/vue3-ssr',
-            '@juggle/resize-observer',
-          ]
-        : ['@juggle/resize-observer'],
-  },
-  vite: {
-    plugins: [
-      Components({
-        resolvers: [NaiveUiResolver()],
-      }),
-    ],
-    optimizeDeps: {
-      include:
-        process.env.NODE_ENV === 'development'
-          ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
-          : [],
-    },
   },
   nitro: {
     // Production
