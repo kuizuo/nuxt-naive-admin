5c78783039f3641ae6b7280e6bf240ef76bd8c45
kuizuo <hi@kuizuo.cn>
2023-03-13 18:27:19 +0800

    chore: optimize typesafe

--- server/api/repo/[username].ts
+++ server/api/repo/[username].ts
@@ -2,7 +2,7 @@ import { Github } from '~~/server/protocol/github'
 import { ResOp } from '~~/server/utils'
 
 export default defineEventHandler(async (event) => {
-  const { username } = event.context.params
+  const { username } = event.context.params as { username: string }
 
   const repos = await Github.getRepos(username)
   return ResOp.success(repos)
