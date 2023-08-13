d6afffd82476adec37ec1929e0f027c123b600da
Kuizuo <hi@kuizuo.cn>
2023-08-14 02:38:09 +0800

    fix: lose style in content page

--- layouts/page.vue
+++ layouts/page.vue
@@ -2,7 +2,7 @@
   <div>
     <AppHeader />
     <div class="max-w-2xl px-4 py-10 my-2 mx-auto bg-white sm:px-8 sm:shadow dark:bg-gray-800 sm:rounded">
-      <main class="prose prose-gray dark:prose-invert max-w-none">
+      <main class="pros dark:prose-zinc max-w-none">
         <slot />
       </main>
     </div>
@@ -10,14 +10,14 @@
   </div>
 </template>
 
-<style>
+<style lang="postcss">
 .prose h2 a,
 .prose h3 a {
-  --at-apply: "no-underline";
+  @apply no-underline
 }
 
 .prose h2 a:hover,
 .prose h3 a:hover {
-  --at-apply: "border-b border-neutral-600 dark:border-neutral-300";
+  @apply no-underline border-b border-neutral-600 dark:border-neutral-300
 }
 </style>
