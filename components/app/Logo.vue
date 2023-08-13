7b6f907961d389cab8ce21c578ac8f533a0e0896
Kuizuo <hi@kuizuo.cn>
2023-08-14 03:17:44 +0800

    chore: optimize header style

--- components/app/Logo.vue
+++ components/app/Logo.vue
@@ -7,8 +7,8 @@ const { title, logo } = useAppConfig()
 
 <template>
   <NuxtLink to="/" class="flex items-center justify-center gap-2">
-    <Icon v-if="/\w+:\w+/.test(logo)" :name="logo" class="w-5 h-5 w-auto" />
-    <img v-else :src="logo" alt="logo" class="w-5 h-5 w-auto">
+    <Icon v-if="/\w+:\w+/.test(logo)" :name="logo" class="w-5 h-5" />
+    <img v-else :src="logo" alt="logo" class="w-5 h-5">
     <h2 v-show="showTitle" class="ml-2 truncate md:opacity-100 font-sans leading-1em text-left text-base capitalize">
       {{ title }}
     </h2>
