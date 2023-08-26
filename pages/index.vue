06f02dfd3ee1f2db12492a36218b3490b3756f17
Kuizuo <hi@kuizuo.cn>
2023-08-26 17:29:45 +0800

    refactor: index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts">
-const username = ref('kuizuo')
+<script setup lang="tsx">
+import { NAvatar, NButton, NEmpty, NList, NListItem, NSpace, NTag, NThing } from 'naive-ui'
 
-const { data: user, pending, execute } = await useLazyAsyncData(async () => {
-  return (await request(`/api/github/user/${username.value}`)).data
+const username = ref('')
+
+const { data: user, pending, execute } = await useAsyncData(async () => {
+  if (!username.value)
+    return null
+  return (await request(`/api/github/user/${username.value}`)).data as Github.User
 })
 
 async function handleQuery() {
@@ -12,6 +16,76 @@ async function handleQuery() {
 function reset() {
   username.value = ''
   user.value = null
+}
+
+function Info({ user }: { user: Github.User }) {
+  return <div>
+    <NAvatar size={100} src={user!.avatar_url} />
+    <h2 class="text-xl font-bold">
+      <a href={user.blog} target="_blank" class="transition hover:text-green-500">
+        {user.name}
+      </a>
+    </h2>
+    <div class="flex justify-center items-center gap-4">
+      <a
+        class="flex flex-col cursor-pointer hover:text-green-600 transition"
+        href={`https://github.com/${user.login}?tab=following`} target="_blank"
+      >
+        <p>{user.following}</p>
+        <p>关注</p>
+      </a>
+      <a
+        class="flex flex-col cursor-pointer hover:text-green-600 transition"
+        href={`https://github.com/${user.login}?tab=followers`} target="_blank"
+      >
+        <p>{user.followers}</p>
+        <p>粉丝</p>
+      </a>
+    </div>
+  </div>
+}
+
+function Repo({ repos }: { repos: Github.Repo[] }) {
+  if (!repos || repos?.length === 0)
+    return <NEmpty description="暂无仓库" />
+
+  return (
+    <div class="mt-2">
+      <NList hoverable clickable bordered>
+        {repos.map(repo => (
+          <NListItem key={repo.id}>
+            {{
+              default: () => <NThing content-style="margin-top: 10px;">
+                {{
+                  default: () => <p class="text-left">
+                    {repo.description}
+                  </p>,
+                  header: () => <div class="inline-flex items-center font-sans">
+                    <h2 class="text-xl">
+                      {repo.name}
+                    </h2>
+                    <a href={repo.html_url} target="_blank" class="ml-1 inline-flex items-center">
+                      <i class="i-ri-star-s-fill inline-block cursor-pointer text-amber-400 mr-1" />
+                      {repo.stargazers_count}
+                    </a>
+                  </div>,
+                  description: () => <NSpace size="small" class="mt-1">
+                    {repo.topics.map(topic => (
+                      <NTag key={topic} bordered={false} type="info" size="small">
+                        {topic}
+                      </NTag>
+                    ))}
+                  </NSpace>,
+                }}
+              </NThing>,
+              suffix: () => <a href={repo.html_url} target="_blank">
+                <NButton type="primary">查看仓库</NButton>
+              </a>,
+            }}
+          </NListItem>
+        ))}
+      </NList>
+    </div>)
 }
 </script>
 
@@ -26,7 +100,7 @@ function reset() {
             </div>
           </NAlert>
           <div class="my-4">
-            <NSpace justify="center">
+            <NSpace justify="center" inline>
               <NInput v-model:value="username" placeholder="请输入要查询github账号" />
               <NButton type="primary" :loading="pending" :disabled="!username" @click="handleQuery()">
                 查询
@@ -36,10 +110,9 @@ function reset() {
               </NButton>
             </NSpace>
           </div>
-
           <div v-if="user?.login">
-            <DemoInfo :user="user" />
-            <DemoRepos :repos="user.repos" />
+            <Info :user="user" />
+            <Repo :repos="user.repos" />
           </div>
         </NCard>
       </div>
